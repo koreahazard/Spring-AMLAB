993c66c6202f8ed4ceb00b07df748d760b597f0a
이형준 <gggudwnsgg@naver.com>
2026-01-04 18:25:32 +0900

    init: 스프링 프로젝트

---
+++ spring-amlab/src/main/java/com/amlab/spring_amlab/SpringAmlabApplication.java
@@ -0,0 +1,13 @@
+package com.amlab.spring_amlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAmlabApplication.class, args);
+	}
+
+}
